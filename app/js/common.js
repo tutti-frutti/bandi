927f3fd73431a1170a055aab011bba04fe0dc60e
tutti-frutti <buzzloud.origin@gmail.com>
2017-05-13 15:48:54 +0300

    next

--- app/js/common.js
+++ app/js/common.js
@@ -2,10 +2,12 @@ $(document).ready(function () {
     $('.h-slider').owlCarousel({
         items: 1,
         loop: true,
-        nav:true,
+        nav:false,
         dots: true,
-//        autoplay: true,
-//        autoplayTimeout: 1000,
-//        autoplayHoverPause: true,
     });
+    // for last word in h2
+    $('.h-slider__h2').each(function(){
+        var ths = $(this);
+        ths.html(ths.html().replace(/(\S+)\s*$/, '<span>$1</span>'));
+    })
 });
