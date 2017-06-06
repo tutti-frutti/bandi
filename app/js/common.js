98987e79c6aafce4533d8f58d805a591e34168ca
tutti-frutti <buzzloud.origin@gmail.com>
2017-06-06 21:48:35 +0300

    adaptive

--- app/js/common.js
+++ app/js/common.js
@@ -10,12 +10,41 @@ $(document).ready(function () {
         loop: true,
         nav: false,
         dots: true,
+        responsiveClass:true,
+        responsive:{
+        0:{
+            items:1,
+        },
+        600:{
+            items:2,
+            nav:false
+        },
+        1000:{
+            items:3,
+            loop:false
+        }
+    }
     });
     $('.team__slider').owlCarousel({
         items: 4,
         loop: true,
         nav: false,
         dots: true,
+        responsiveClass:true,
+            center: true,
+        responsive:{
+        0:{
+            items:1,
+        },
+        600:{
+            items:2,
+            nav:false,
+        },
+        1000:{
+            items:3,
+            loop:false
+        }
+    }
     });
     // for last word in h2
     $('.h-slider__h2').each(function () {
